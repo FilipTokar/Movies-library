fceaab6acce61622dc56cfb1797b82024ecfe240
Filip <filip.tokar@seznam.cz>
2023-04-17 15:59:33 +0200

    adding API call as a promise and implementing a loading state

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-// document.querySelector("#input__btn").onclick = function getInput() {
-//     return document.querySelector("#input__field").value
-//     }
 
-function getInput() {
+
+async function renderMovies() {
     let input = document.querySelector("#input__field").value
-    renderMovies(input)
-} 
-
-async function renderMovies (apiRoute) {
+    const moviesListElement = document.querySelector(".movies-list")
+    const searchTitleElement = document.querySelector(".search__title")
     
-    const movies = await fetch(`http://www.omdbapi.com/?apikey=56cb40ec&s=${apiRoute}`)
+    moviesListElement.classList += " movies__loading"
+
+    let movies = await getMovies(input)
     const moviesData = await movies.json()
     const firstSixMovies = moviesData.Search.slice(0,6)
-    const moviesListElement = document.querySelector(".movies-list")
 
+    moviesListElement.classList.remove("movies__loading")
+    
+    searchTitleElement.innerHTML += ` for: <span class="search__title--black">${input}</span>`
     moviesListElement.innerHTML = firstSixMovies.map((movie) => movieHTML(movie)).join("")
-}
 
-
-
-
-function movieHTML (movie) {
-    return `<div class="movie">
-    <div class="movie-card">
-        <div class="movie-card__container">
-            <figure class="movie__img--container">
-                <img class="movie__img" src="${movie.Poster}" alt="">
-            </figure>
-            <p class="movie__para"><b>Title:</b> ${movie.Title}</p>
-            <p class="movie__para"><b>Year:</b> ${movie.Year}</p>
+    function movieHTML (movie) {
+        return `<div class="movie">
+        <div class="movie-card">
+            <div class="movie-card__container">
+                <figure class="movie__img--container">
+                    <img class="movie__img" src="${movie.Poster}" alt="">
+                </figure>
+                <p class="movie__para"><b>Title:</b> ${movie.Title}</p>
+                <p class="movie__para"><b>Year:</b> ${movie.Year}</p>
+            </div>
         </div>
-    </div>
-</div>`
+    </div>`
+    }
+} 
+
+function getMovies (apiRoute) {
+    return new Promise ((resolve) => {
+        resolve(fetch(`http://www.omdbapi.com/?apikey=56cb40ec&s=${apiRoute}`))  
+    })  
 }
+
 
 function clickInputButton (event) {
     let button = document.querySelector("#input__btn")
